e26e88823f9b2a605c1b23f1deec6b347c411b6d
Vicente Ferrer <ferrergallardovicente@gmail.com>
2025-09-18 19:04:38 +0200

    Clase Disco duro completada

---
+++ src/DiscoDuro.ts
@@ -0,0 +1,37 @@
+import { AlmacenarDatos } from "./AlmacenarDatos.js";
+import { Disco } from "./Disco.js";
+
+class DiscoDuro extends Disco implements AlmacenarDatos{
+    #tipoConexion: string;
+    #formateado: boolean;
+
+    constructor(nombre:string, contenido: string, capacidad:number, tipo: string, rpm: number,tipoConexion; String, formateado: boolean){
+            super(nombre, contenido, capacidad, tipo, rpm);
+            this.#tipoConexion = tipoConexion;
+            this.#formateado = formateado;
+    }
+
+    getTipoConexion() {return this.#tipoConexion;}
+    setTipoConexion(nuevoTipoConexion:string) {this.#tipoConexion = nuevoTipoConexion}
+    
+    getformateado(){ return this.#formateado;}
+    setFormateado(estaFormateado: boolean) { this.#formateado = estaFormateado;}
+
+    leerDatos(): void {
+        console.log("Disco duro escribiendo con cabezal magnetico:");
+    }
+
+    escribirDatos(datos: string): void {
+        console.log(`Disco duro escribiendo con cabezal magnteico: ${datos}`)
+        this.setContenido(datos);
+    }
+
+    formatear():void {
+        this.#formateado = true;
+        console.log("Formateo completado.");
+    }
+
+    conectar(): void {
+        console.log(`Conectado por ${this.#tipoConexion}`)
+    }
+}
