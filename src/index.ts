1e5cd3a024af189a90a407b9ee4e07577ad26e83
Vicente Ferrer <ferrergallardovicente@gmail.com>
2025-09-19 13:20:41 +0200

    Index terminado

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hola Mundo");
+import { Cd } from "./CD.js";
+import { DiscoDuro } from "./DiscoDuro.js";
+
+const cd = new Cd ("Julio Iglesias", "Grabado", 700, "CD_R", 400, false);
+const hdd = new DiscoDuro ("DiscoDuroHdd","Con datos", 500_000, "HDD", 7200, "SATA", true);
+
+
+cd.girarDisco();
+cd.leerDatos();
+// cd.escribirDatos("Nuevos datos")
+cd.reportarInformacion();
+cd.expulsar();
+
+
+// DISCO DURO
+
+hdd.girarDisco();
+hdd.conectar();
+hdd.leerDatos();
+hdd.escribirDatos("Entregable Vicente");
+hdd.formatear();
+hdd.reportarInformacion();
